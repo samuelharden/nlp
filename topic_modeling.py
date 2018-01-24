e6af82230fd82f5ade2f2ea73236f72a76f781c6
Gaurav Arora <gauravarora.daiict@gmail.com>
2018-01-24 20:35:45 +0530

    Update topic modeling

--- topic_modeling.py
+++ topic_modeling.py
@@ -7,21 +7,24 @@ import os
 import json
 from gensim import corpora
 
-class MyCorpus(object):
-    def __iter__(self):
-        files = os.listdir('/home/irlab/research/processed_papers/')
+class MyCorpus(corpora.TextCorpus):
+    def get_texts(self):
+        files = os.listdir('../processed_papers_t/')
         text = ''
         json_data={}
         for fl in files:
             print fl
-            json_data = json.load(open('/home/irlab/research/processed_papers/'+fl))
+            json_data = json.load(open('../processed_papers_t/'+fl))
             for  val in json_data["abstract_sentences"].values():
                 text += " "
-                text += val
+		if val is not None:
+		    text += val
             for val in json_data['body_sentences'].values():
                 text += " "
-                text += val
-        yield dictionary.doc2bow(text.lower().split())
+		if val is not None:
+		    text += val
+	    #print text
+	    yield self.preprocess_text(text)
 
 stop = stopwords.words('english')
 add_stopwords = ['said', 'mln', 'billion', 'million', 'pct', 'would', 'inc', 'company', 'corp']
@@ -66,11 +69,14 @@ def run():
     docs = remove_infrequent_words(tokenized_docs)
 
     # Create dictionary and corpus
-    dictionary = corpora.Dictionary(docs)
-    corpus = [dictionary.doc2bow(doc) for doc in docs]
-
+    #corpus = [dictionary.doc2bow(doc) for doc in docs]
+    corpus = MyCorpus()
+    print corpus.get_texts()
+    dictionary = corpora.Dictionary()
+    dictionary.add_documents(corpus.get_texts())
+    #corpus.init_dictionary(dictionary)
     # Build LDA model
-    lda = models.LdaModel(corpus, id2word=dictionary, num_topics=10)
+    lda = models.LdaModel(corpus, id2word=dictionary, num_topics=100, update_every=0, chunksize=3000, passes=20)
     for topic in lda.show_topics():
         print topic
 
