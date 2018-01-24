ecbee3a96792d5acc14754d54e8ae54c33ff63d7
Gaurav Arora <gauravarora.daiict@gmail.com>
2018-01-24 18:50:16 +0530

    Modify

--- summarization/summarize_text.py
+++ summarization/summarize_text.py
@@ -1,4 +1,4 @@
-emport os
+import os
 import re
 import pickle
 import nltk
@@ -28,16 +28,18 @@ def clean_document(document):
     document = re.sub('[^A-Za-z .-]+', ' ', document)
     document = document.replace('-', '')
     document = document.replace('...', '')
+    document = document.replace('...', '')
     document = document.replace('Mr.', 'Mr').replace('Mrs.', 'Mrs')
 
     # Remove Ancronymns M.I.T. -> MIT
     # to help with sentence tokenizing
     document = merge_acronyms(document)
 
+    document = document.lower()
+    document = remove_stop_words(document)
+
     # Remove extra whitespace
     document = ' '.join(document.split())
-    document.lower()
-    remove_stop_words(document)
     return document
 
 def remove_stop_words(document):
@@ -45,68 +47,19 @@ def remove_stop_words(document):
     document = ' '.join([i for i in document.split() if i not in stop])
     return document
 
-def similarity_score(t, s):
-    """Returns a similarity score for a given sentence.
-
-    similarity score = the total number of tokens in a sentence that exits
-                        within the title / total words in title
-
-    """
-    t = remove_stop_words(t.lower())
-    s = remove_stop_words(s.lower())
-    t_tokens, s_tokens = t.split(), s.split()
-    similar = [w for w in s_tokens if w in t_tokens]
-    score = (len(similar) * 0.1 ) / len(t_tokens)
-    return score
-
 def merge_acronyms(s):
-    """Merges all acronyms in a given sentence. For example M.I.T -> MIT"""
+    """Merges all acronyms in a given sentence. For example M.I.T -> MIT
+    """
     r = re.compile(r'(?:(?<=\.|\s)[A-Z]\.)+')
     acronyms = r.findall(s)
     for a in acronyms:
         s = s.replace(a, a.replace('.',''))
     return s
 
-def rank_sentences(doc, abs_sent, doc_matrix, feature_names, top_n=2):
-    """Returns top_n sentences. Theses sentences are then used as summary
-    of document.
-
-    input
-    ------------
-    doc : a document as type str
-    doc_matrix : a dense tf-idf matrix calculated with Scikits TfidfTransformer
-    feature_names : a list of all features, the index is used to look up
-                    tf-idf scores in the doc_matrix
-    top_n : number of sentences to return
-
-    """
-    sents = nltk.sent_tokenize(doc)
-    sentences = [nltk.word_tokenize(sent) for sent in sents]
-    sentences = [[w for w in sent if nltk.pos_tag([w])[0][1] in NOUNS]
-                  for sent in sentences]
-    tfidf_sent = [[doc_matrix[feature_names.index(w.lower())]
-                   for w in sent if w.lower() in feature_names]
-                 for sent in sentences]
-    # Calculate Sentence Values
-    doc_val = sum(doc_matrix)
-    sent_values = [sum(sent) / doc_val for sent in tfidf_sent]
-
-    # Apply Similariy Score Weightings
-    similarity_scores = [similarity_score(abs_sent, sent) for sent in sents]
-    scored_sents = np.array(sent_values) + np.array(similarity_scores)
-
-    # Apply Position Weights
-    ranked_sents = [sent*(i/len(sent_values))
-                    for i, sent in enumerate(sent_values)]
-
-    ranked_sents = [pair for pair in zip(range(len(sent_values)), sent_values)]
-    ranked_sents = sorted(ranked_sents, key=lambda x: x[1] *-1)
-
-    return ranked_sents[:top_n]
-
 if __name__ == '__main__':
     # Load corpus data used to train the TF-IDF Transformer
     data = pickle.load(open('data.pkl', 'rb'))
+    data = [clean_document(da) for da in data]
     train_data = set(data)
     #print train_data
     # Fit and Transform the term frequencies into a vector
@@ -122,11 +75,6 @@ if __name__ == '__main__':
     files = os.listdir('../..//processed_papers/')
     abstract = ''
     body = ''
-    # Get the dense tf-idf matrix for the document
-    #story_freq_term_matrix = count_vect.transform([doc])
-    #story_tfidf_matrix = tfidf.transform(story_freq_term_matrix)
-    #story_dense = story_tfidf_matrix.todense()
-    #doc_matrix = story_dense.tolist()[0]
     json_array = {}
     for file in files:
         json_internal_array = {}
@@ -152,11 +100,15 @@ if __name__ == '__main__':
                 body += section
         title =  abstract
         document =  body
-        abs_freq_term_matrix = count_vect.transform(list(abs_sentences.values()))
+	abs_sentences_value = list(abs_sentences.values())
+	abs_sentences_value = [ clean_document(abs_value) for abs_value in abs_sentences_value]
+        abs_freq_term_matrix = count_vect.transform(abs_sentences_value)
 	abs_sentences_idx = list(abs_sentences.keys())
         abs_tfidf_matrix = tfidf.transform(abs_freq_term_matrix)
         #abs_dense = abs_tfidf_matrix.todense()
-        body_freq_term_matrix = count_vect.transform(list(body_sentences.values()))
+	body_sentences_value = list(body_sentences.values())
+	body_sentences_value = [ clean_document(body_value) for body_value in body_sentences_value]
+        body_freq_term_matrix = count_vect.transform(body_sentences_value)
 	body_sentences_idx = list(body_sentences.keys())
         body_tfidf_matrix = tfidf.transform(body_freq_term_matrix)
         #body_dense = body_tfidf_matrix.todense()
